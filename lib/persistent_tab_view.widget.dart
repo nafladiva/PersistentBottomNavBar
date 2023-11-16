cba71c83dd684cbf88069b3ca1b1928cc688deca
Nafla Diva Syafia <nafladiva@gmail.com>
2023-11-16 21:47:00 +0700

    feat: add popAllScreensOnTapOfSelectedTab in PersistentTabView.custom

--- lib/persistent_tab_view.widget.dart
+++ lib/persistent_tab_view.widget.dart
@@ -103,6 +103,7 @@ class PersistentTabView extends PersistentTabViewBase {
     this.hideNavigationBar,
     this.screenTransitionAnimation = const ScreenTransitionAnimation(),
     final double navBarHeight = kBottomNavigationBarHeight,
+    final bool popAllScreensOnTapOfSelectedTab = false,
   })  : assert(itemCount == screens.length,
             "screens and items length should be same. If you are using the onPressed callback function of 'PersistentBottomNavBarItem', enter a dummy screen like Container() in its place in the screens"),
         assert(handleAndroidBackButtonPress && onWillPop != null,
@@ -128,6 +129,7 @@ class PersistentTabView extends PersistentTabViewBase {
           screenTransitionAnimation: screenTransitionAnimation,
           isCustomWidget: true,
           navBarHeight: navBarHeight,
+          popAllScreensOnTapOfSelectedTab: popAllScreensOnTapOfSelectedTab,
           decoration: const NavBarDecoration(),
         );
 
